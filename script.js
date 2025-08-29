16d9b92f982e102d51257ead8d00045f61b17db9
JeongHee Yoon <jeongheeyoon@JeongHee-MacBook-M2-Max.local>
2025-08-29 23:15:51 +0900

    구글 설문 폼을 반응형으로 개선

--- script.js
+++ script.js
@@ -3,9 +3,4 @@ document.getElementById('menuBtn').addEventListener('click', function(){
   const nav = document.getElementById('mobileNav');
   nav.classList.toggle('hidden');
 });
-// Fake submit
-document.getElementById('leadForm').addEventListener('submit', function(e){
-  e.preventDefault();
-  document.getElementById('formSuccess').classList.remove('hidden');
-  e.target.reset();
-});
+
