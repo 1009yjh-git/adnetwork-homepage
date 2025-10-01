45d3a2a420c1b40abfc3d2dafb1e1e8b99d9439e
JeongHee Yoon <jeongheeyoon@JeongHee-MacBook-M2-Max.local>
2025-10-01 18:55:29 +0900

    커밋

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Mobile Nav Toggle
+document.getElementById('menuBtn').addEventListener('click', function(){
+  const nav = document.getElementById('mobileNav');
+  nav.classList.toggle('hidden');
+});
+
